6bb4ef79877d496d708fd6b4f47ee7875ca1587a
IperEnma <enmanuelhernandez1843@gmail.com>
2022-04-11 05:58:39 -0700

    exit

--- funcexit.c
+++ funcexit.c
@@ -6,42 +6,27 @@
  */
 int funexit(char *buffer)
 {
-	char *exit = "exit";
-	int i = 0, j = 0;
+	char *exit;
+	int i = 0, j = 0, m = 0;
 
 	for (i = 0; buffer[i]; i++)
 	{
-		if (buffer[i] == ' ')
-			;
-		else
+		if (buffer[i] != ' ')
+			m++;
+	}
+
+	exit = malloc((m) + 1);
+
+	for (i = 0; buffer[i]; i++)
+	{
+		if (buffer[i] != ' ')
 		{
-			if (buffer[i] == exit[j])
-			{
-				while (j < 5)
-				{
-				if (j == 4)
-				{
-					while (j < 5)
-					{
-					i++;
-					if (buffer[i] == '\0')
-						return (1);
-					if (buffer[i] != ' ')
-						return (0);
-					}
-				}
-					if (buffer[i] == exit[j])
-					{
-						i++;
-						j++;
-					}
-					else
-						return (0);
-				}
-			}
-			else
-				return (0);
+			exit[j] = buffer[i];
+			j++;
 		}
 	}
+	exit[j] = '\0';
+	if(strcmp(exit, "exit") == 0)
+		return (1);
 	return (0);
 }
